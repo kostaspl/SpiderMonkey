93a0448f23af3c99d1202e8349041bf7ff2253e8
Kostas Plelis <plelis@ics.forth.gr>
2015-11-03 15:15:05 -0800

    Added addl_ir(imm, dst)

--- js/src/jit/shared/BaseAssembler-x86-shared.h
+++ js/src/jit/shared/BaseAssembler-x86-shared.h
@@ -267,6 +267,28 @@ public:
 
     void addl_ir(int32_t imm, RegisterID dst)
     {
+        if (shouldBlindConstant(imm))
+            addl_ir_blnd(imm, dst);
+        else
+            addl_ir_norm(imm, dst);
+    }
+
+    void addl_ir_blnd(int32_t imm, RegisterID dst)
+    {
+        int bv;
+        if (CAN_SIGN_EXTEND_8_32(imm)) {
+            bv = blindingValue8();
+            addl_ir_norm(imm - bv, dst);
+            addl_ir_norm(bv, dst);
+        } else {
+            bv = blindingValue();
+            addl_ir_norm(imm - bv, dst);
+            addl_ir_norm(bv, dst);
+        }
+    }
+
+    void addl_ir_norm(int32_t imm, RegisterID dst)
+    {
         spew("addl       $%d, %s", imm, GPReg32Name(dst));
         if (CAN_SIGN_EXTEND_8_32(imm)) {
             m_formatter.oneByteOp(OP_GROUP1_EvIb, dst, GROUP1_OP_ADD);
