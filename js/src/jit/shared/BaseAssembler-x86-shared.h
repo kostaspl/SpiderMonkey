f9938068935aa4bc63d2cde2e1be85431767e0ce
Kostas Plelis <plelis@ics.forth.gr>
2015-11-03 14:19:52 -0800

    Added xorq_ir(imm, dst)

--- js/src/jit/shared/BaseAssembler-x86-shared.h
+++ js/src/jit/shared/BaseAssembler-x86-shared.h
@@ -974,6 +974,28 @@ public:
 
     void xorq_ir(int32_t imm, RegisterID dst)
     {
+        if (shouldBlindConstant(imm))
+            xorq_ir_blnd(imm, dst);
+        else
+            xorq_ir_norm(imm, dst);
+    }
+
+    void xorq_ir_blnd(int32_t imm, RegisterID dst)
+    {
+        int bv;
+        if (CAN_SIGN_EXTEND_8_32(imm)) {
+            bv = blindingValue8();
+            xorq_ir_norm(imm ^ bv, dst);
+            xorq_ir_norm(bv, dst);
+        } else {
+            bv = blindingValue();
+            xorq_ir_norm(imm ^ bv, dst);
+            xorq_ir_norm(bv, dst);
+        }
+    }
+
+    void xorq_ir_norm(int32_t imm, RegisterID dst)
+    {
         spew("xorq       $0x%" PRIx64 ", %s", int64_t(imm), GPReg64Name(dst));
         if (CAN_SIGN_EXTEND_8_32(imm)) {
             m_formatter.oneByteOp64(OP_GROUP1_EvIb, dst, GROUP1_OP_XOR);
